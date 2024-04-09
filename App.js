e33c61b01d372bd1747c115a1987ade809b536cc
dmitrypilipenko91 <dmitrypilipenko91@gmail.com>
2024-04-09 10:02:08 +0300

    Installed react-router-native, implemented the routes system, made StartPage

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
+import { NativeRouter, Routes, Route, Navigate } from 'react-router-native';
 import Navbar from './components/Navbar';
 import PokemonList from './components/PokemonList';
+import StartPage from './pages/StartPage';
 import { store } from './store';
 import { styles } from './styles';
 
@@ -10,10 +12,12 @@ export default function App() {
   
   return (
     <Provider store={store}>
-      <View style={styles.container}>
-        <Navbar></Navbar>
-        <PokemonList></PokemonList>
-      </View>
+      <NativeRouter>
+        <Routes>
+          <Route path='/startpage' element={<StartPage/>}/>
+          <Route path='/*' element={<Navigate to='/startpage' replace/>}/>
+        </Routes>
+      </NativeRouter>
     </Provider>  
   );
 }
---
+++ pages/StartPage.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View } from 'react-native';
+import { Provider } from 'react-redux';
+import Navbar from '../components/Navbar';
+import PokemonList from '../components/PokemonList';
+import { store } from '../store';
+import { styles } from '../styles';
+
+
+export default function StartPage() {
+  
+  return (
+    <Provider store={store}>
+      <View style={styles.container}>
+        <Navbar></Navbar>
+        <PokemonList></PokemonList>
+      </View>
+    </Provider>  
+  );
+}
